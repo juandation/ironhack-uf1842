998115dec6b2ab2282f7d8bf6ed4601061dd3dba
Juan David Ospina <juandation@gmail.com>
2025-04-22 14:29:22 +0200

    algoritmia solved

--- algoritmia/1-combine-names.js
+++ algoritmia/1-combine-names.js
@@ -1,14 +1,14 @@
 /**
  * Crea una función que combine los dos nombres, dejando un espacio en medio
- * 
+ *
  * @param {string} name1 Primer nombre
  * @param {string} name2 Segundo nombre
- * 
- * @returns {string} Devuelve un string que son los dos nombres contactenados separados por un espacio 
+ *
+ * @returns {string} Devuelve un string que son los dos nombres contactenados separados por un espacio
  */
 
 function combineNames(name1, name2) {
-
+	return `${name1} ${name2}`;
 }
 
 console.log(combineNames('James', 'Stevens')); // "James Stevens"
--- algoritmia/2-match-uefa-euro.js
+++ algoritmia/2-match-uefa-euro.js
@@ -1,17 +1,22 @@
-/** 
- * Disponemos de una función que debe formatear un string, en función del resultado de un partido de fútbol (es para una página Web deportiva). 
- * 
- * 
+/**
+ * Disponemos de una función que debe formatear un string, en función del resultado de un partido de fútbol (es para una página Web deportiva).
+ *
+ *
  * @param {array<string>} teams Los competidores del partido, en un array de strings, siempre de dos posiciones
  * @param {array<number>} scores Las goles que han metido cada equipo, respectivamente.
- * 
- * @returns {string} Resultado del partido, siguiendo el formato indicado en los comentarios, según si ha ganado el primer equipo, el segundo, o ha habido empate.   
-*/
+ *
+ * @returns {string} Resultado del partido, siguiendo el formato indicado en los comentarios, según si ha ganado el primer equipo, el segundo, o ha habido empate.
+ */
 function uefaEuro2016(teams, scores) {
-
+	return `At match ${teams[0]} - ${teams[1]}, ${
+		scores[0] > scores[1]
+			? teams[0] + ' won!'
+			: scores[0] < scores[1]
+			? teams[1] + ' won!'
+			: 'teams played draw.'
+	}`;
 }
-
-console.log(uefaEuro2016(['Germany', 'Ukraine'], [2, 0])) // "At match Germany - Ukraine, Germany won!"
-console.log(uefaEuro2016(['Belgium', 'Italy'], [0, 2])) // "At match Belgium - Italy, Italy won!"
-console.log(uefaEuro2016(['Portugal', 'Iceland'], [1, 1])) // "At match Portugal - Iceland, teams played draw."
-console.log(uefaEuro2016(['Italy', 'Spain'], [2, 0])) // "At match Italy - Spain, Italy won!"
+console.log(uefaEuro2016(['Germany', 'Ukraine'], [2, 0])); // "At match Germany - Ukraine, Germany won!"
+console.log(uefaEuro2016(['Belgium', 'Italy'], [0, 2])); // "At match Belgium - Italy, Italy won!"
+console.log(uefaEuro2016(['Portugal', 'Iceland'], [1, 1])); // "At match Portugal - Iceland, teams played draw."
+console.log(uefaEuro2016(['Italy', 'Spain'], [2, 0])); // "At match Italy - Spain, Italy won!"
